919b5ddc83bf8940c04f1af4998cc39d2557802c
Brandyn Britton <Brandynbb96@gmail.com>
2024-11-13 10:31:37 +0800

    update: update formula with new version and checksum

--- git-branch-tidy.rb
+++ git-branch-tidy.rb
@@ -2,8 +2,8 @@ class GitBranchTidy < Formula
   desc "A script to clean up local Git branches that have been merged and whose remote branches no longer exist"
   homepage "https://github.com/goodpie/homebrew-scripts"
   url "https://github.com/goodpie/homebrew-scripts/archive/v1.0.0.tar.gz"
-  sha256 "5904075b0c2951873aa618c3b6dd40dc3c281b3c49e5e1759f093746d17efda2"
-  version "1.0.0"
+  sha256 "933cf62426f0d2c1ba1bcfed8f3fad8f1e2479b3b5b694319bfd962791ec2ddc"
+  version "1.0.1"
 
   def install
     bin.install "git-branch-tidy.sh" => "git-branch-tidy"

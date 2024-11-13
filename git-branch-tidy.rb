99d9f61c20206c6cbd162752d40340d8cb6d0ccc
Brandyn Britton <Brandynbb96@gmail.com>
2024-11-13 10:26:52 +0800

    update: fixed template brew formula to add checksum

--- git-branch-tidy.rb
+++ git-branch-tidy.rb
@@ -2,7 +2,7 @@ class GitBranchTidy < Formula
   desc "A script to clean up local Git branches that have been merged and whose remote branches no longer exist"
   homepage "https://github.com/goodpie/homebrew-scripts"
   url "https://github.com/goodpie/homebrew-scripts/archive/v1.0.0.tar.gz"
-  sha256 "your_tarball_sha256_checksum"
+  sha256 "5904075b0c2951873aa618c3b6dd40dc3c281b3c49e5e1759f093746d17efda2"
   version "1.0.0"
 
   def install

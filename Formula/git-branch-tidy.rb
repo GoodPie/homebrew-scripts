2220a477dd423481958a0091d147baeee6eca60f
Brandyn Britton <Brandynbb96@gmail.com>
2025-03-27 18:25:56 +0800

    update: formulas now reference master branch

--- Formula/git-branch-tidy.rb
+++ Formula/git-branch-tidy.rb
@@ -1,11 +1,13 @@
 class GitBranchTidy < Formula
   desc "A script to clean up local Git branches that have been merged and whose remote branches no longer exist"
   homepage "https://github.com/goodpie/git-branch-tidy"
-  url "https://github.com/GoodPie/git-branch-tidy/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f7d9f1dd1f380767a7fc38ea8f3e5fd3fa6a573dceacd69bb388738e4fc4ee91"
-  version "1.0.2"
+  url "https://github.com/GoodPie/git-branch-tidy.git", branch: "master"
+  version "head"
+  head "https://github.com/GoodPie/git-branch-tidy.git", branch: "master"
 
   def install
     bin.install "git-branch-tidy.sh" => "git-branch-tidy"
+
+    chmod 0755, bin/"git-branch-tidy"
   end
 end
--- Formula/pg-docker-helper.rb
+++ Formula/pg-docker-helper.rb
@@ -1,26 +1,16 @@
 class PgDockerHelper < Formula
   desc "A collection of scripts to help import and export Postgres databases from Docker containers. Primarily used for development and testing migrations."
   homepage "https://github.com/GoodPie/export-pg-db-docker"
-  url "https://github.com/GoodPie/export-pg-db-docker/archive/refs/tags/v1.0.2.tar.gz"
-  version "1.0.2"
-
-  resource "import-pg-db-docker" do
-    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/2ce9c22e1a3c98039830b39dcdfa4841db14948c/import-pg-db-docker.sh"
-    sha256 "331690aa5ae2e71f5e1c88cbd2642f3c47716ab19facdd2d5658fc7206a6ff88"
-  end
-
-  resource "export-pg-db-docker" do
-    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/2ce9c22e1a3c98039830b39dcdfa4841db14948c/export-pg-db-docker.sh"
-    sha256 "cbe332557d01737b223fc047a08307b973395f11da67c5130fa23c30accc99bb"
-  end
+  url "https://github.com/GoodPie/export-pg-db-docker.git", branch: "master"
+  version "head"
+  head "https://github.com/GoodPie/export-pg-db-docker.git", branch: "master"
 
   def install
-    resource("import-pg-db-docker").stage do
-      bin.install "import-pg-db-docker.sh" => "import-pg-db-docker"
-    end
-
-    resource("export-pg-db-docker").stage do
-      bin.install "export-pg-db-docker.sh" => "export-pg-db-docker"
-    end
+    bin.install "import-pg-db-docker.sh" => "import-pg-db-docker"
+    bin.install "export-pg-db-docker.sh" => "export-pg-db-docker"
+    
+    # Make them executable
+    chmod 0755, bin/"import-pg-db-docker"
+    chmod 0755, bin/"export-pg-db-docker"
   end
 end

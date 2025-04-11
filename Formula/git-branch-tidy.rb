f3fcf5ebf758d7b4e7f0bb84cf5915ee40ded998
Brandyn Britton <Brandynbb96@gmail.com>
2025-04-11 08:18:48 +0800

    Update git-branch-tidy.rb

--- Formula/git-branch-tidy.rb
+++ Formula/git-branch-tidy.rb
@@ -1,9 +1,9 @@
 class GitBranchTidy < Formula
   desc "A script to clean up local Git branches that have been merged and whose remote branches no longer exist"
   homepage "https://github.com/goodpie/git-branch-tidy"
-  url "https://github.com/GoodPie/git-branch-tidy.git", branch: "master"
+  url "https://github.com/GoodPie/git-branch-tidy.git", branch: "main"
   version "head"
-  head "https://github.com/GoodPie/git-branch-tidy.git", branch: "master"
+  head "https://github.com/GoodPie/git-branch-tidy.git", branch: "main"
 
   def install
     bin.install "git-branch-tidy.sh" => "git-branch-tidy"

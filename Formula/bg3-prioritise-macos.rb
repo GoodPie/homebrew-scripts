d740f8b15dbd95809bde7a47822e3d54252f32a2
Brandyn Britton <Brandynbb96@gmail.com>
2025-05-03 20:13:23 +0800

    update: fix typo in formula

--- Formula/bg3-prioritise-macos.rb
+++ Formula/bg3-prioritise-macos.rb
@@ -1,4 +1,4 @@
-class BG3PrioritiseMacOs < Formula
+class BG3PrioritiseMacos < Formula
   desc "Prioritise BG3 process on MacOS"
   homepage "https://github.com/GoodPie/bg3-prioritise-macos"
   url "https://github.com/GoodPie/bg3-prioritise-macos.git", branch: "main"
